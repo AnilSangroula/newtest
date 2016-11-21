89700b017a145ffc68a011a20c6d1ecd0470e642
Sangroula <asang002@int.usc.local>
2016-11-21 10:03:33 -0600

    Adding the files in the github

---
+++ testgit/src/main/java/com/testgit/TestName.java
@@ -0,0 +1,14 @@
+package com.testgit;
+
+public class TestName {
+	public int add(int a, int b){
+		int result = a + b;
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
